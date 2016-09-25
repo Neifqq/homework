7468baefc2171b8f8d0c20e30404241e4a2bd431
Neif <Neif@DESKTOP-9DLDT0H>
2016-09-26 01:16:54 +0300

    Часть 3, задача 19.

---
+++ ConsoleApplication8/ConsoleApplication8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            do
+            {
+                x = int.Parse(Console.ReadLine());
+                if (x > 0)
+                {
+                    int s = 0, k = 1;
+                    while (k < x)
+                    {
+                        k *= 2;
+                        s++;
+                        
+                    }
+                    Console.WriteLine("Ответ: {0}",Math.Pow(2, s));
+                }
+                else
+                {
+                    Console.WriteLine("Ответ: {0}",Math.Abs(x));
+                }
+            }
+            while (x != 0);
+        }
+    }
+}
