954cf89ad19883b2f81df1b5f040ae514d12fe0f
Neif <Neif@DESKTOP-9DLDT0H>
2016-09-26 00:21:51 +0300

    Часть 2, задача 19.

---
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n16 = Console.ReadLine(), nk;
+            long k = long.Parse(Console.ReadLine());
+            int[] ch = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            long n10 = Convert.ToInt64(n16, 16);
+            do
+            {
+                long l = n10 % k;
+                nk = ch[l] + n16;
+                n10 = (long)(n10 / k);
+            }
+            while (n10 > 0);
+            Console.WriteLine(nk);
+        }
+    }
+}
