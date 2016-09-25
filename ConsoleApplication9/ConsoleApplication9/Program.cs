47f849a6080db9ce2ace2cc5ebc6394d91e4d157
Neif <Neif@DESKTOP-9DLDT0H>
2016-09-26 01:27:50 +0300

    Часть 3, задача 20.

---
+++ ConsoleApplication9/ConsoleApplication9/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, plus = 0, minus = 0, p = 0; // p - разница между количеством положительных и отрицательных чисел.
+            do
+            {
+                x = int.Parse(Console.ReadLine());
+                if (x < 0)
+                {
+                    p--;
+                    minus += x;
+                }
+                if (x > 0)
+                {
+                    p++;
+                    plus += x;
+                }
+            }
+            while (x != 0);
+            if (p == 0)
+            {
+                Console.WriteLine("Условиями задачи такой результат не предусмотрен :(");
+            }
+            else
+            {
+                if (p < 0)
+                {
+                    Console.WriteLine(minus);
+                }
+                else
+                {
+                    Console.WriteLine(plus);
+                }
+            }
+        }
+    }
+}
