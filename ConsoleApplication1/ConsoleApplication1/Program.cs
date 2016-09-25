39801fbef038f8c3f1c99e7d0059270f32173c22
Neif <Neif@DESKTOP-9DLDT0H>
2016-09-25 22:01:21 +0300

    Часть 1, 10.

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine()), b = int.Parse(Console.ReadLine()), c = int.Parse(Console.ReadLine());
+            if (a == 0 & b == 0 & c == 0)
+            {
+                Console.WriteLine("-1");
+            }
+            else
+            {
+                double D = (b * b - 4 * a * c);
+                if (D >= 0)
+                {
+                    if (D == 0)
+                    {
+                        Console.WriteLine(-b / (2 * a));
+                    }
+                    else
+                    {
+                        Console.WriteLine((-b - Math.Sqrt(D)) / (2 * a));
+                        Console.WriteLine((-b + Math.Sqrt(D)) / (2 * a));
+                    }
+                }
+            }
+        }
+    }
+}
