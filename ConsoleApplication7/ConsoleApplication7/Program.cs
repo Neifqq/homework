6fd8f5785cb173f8c3ed6ba392a0c9a1d2a39a6a
Neif <Neif@DESKTOP-9DLDT0H>
2016-09-26 01:08:34 +0300

    Часть 3, задача 18.

---
+++ ConsoleApplication7/ConsoleApplication7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 0, x;
+            do
+            {
+                x = int.Parse(Console.ReadLine());
+                while (x % 2 == 0 && x > 1)
+                {
+                    s++;
+                    x = x / 2;
+                }
+            }
+            while (x != 0);
+            Console.WriteLine("Степеней двойки: {0}",s);
+        }
+    }
+}
