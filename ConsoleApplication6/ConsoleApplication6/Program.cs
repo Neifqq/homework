541f10ff57d3fdc8eeffdafc8776795aae60114b
Neif <Neif@DESKTOP-9DLDT0H>
2016-09-26 00:56:47 +0300

    Часть 2, задача 20.

---
+++ ConsoleApplication6/ConsoleApplication6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string s = Console.ReadLine();
+            // int k = int.Parse(Console.ReadLine());
+            // int S = Convert.ToInt32(s, k);
+            // double r = S;
+            // Если задаётся основание.
+            double r = double.Parse(Console.ReadLine());
+            do
+            {
+                if (r % 2 == 0)
+                {
+                    Console.Write("0");
+                }
+                r = Math.Floor(r / 2);
+            }
+            while (r > 0);
+            Console.WriteLine("");
+        }
+    }
+}
