cc5e6eb82666ad5e9138b808d2c66e9533cec384
Neif <Neif@DESKTOP-9DLDT0H>
2016-09-25 22:43:23 +0300

    Часть 1, задача 20.

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine()), y = double.Parse(Console.ReadLine());
+            int a = 0, b = 0;
+            for (short k = 1; k <= 6; ++k)
+            {
+                if (k<=3)
+                {
+                    a += Convert.ToInt32(x % 10 + y % 10);
+                    x = Math.Floor(x / 10);
+                    y = Math.Floor(y / 10);
+                }
+                else
+                {
+                    b += Convert.ToInt32(x % 10 + y % 10);
+                    x = Math.Floor(x / 10);
+                    y = Math.Floor(y / 10);
+                }
+            }
+            if (a==b)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
