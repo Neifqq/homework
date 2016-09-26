4fb937e8478001ff94879382930aae21ba520f38
Neif <Neif@DESKTOP-9DLDT0H>
2016-09-26 17:36:54 +0300

    Часть 4, задача 19.

---
+++ ConsoleApplication10/ConsoleApplication10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = int.Parse(Console.ReadLine()), s = 0;
+            long p;
+            for (p = 3; s <= k; p += 2)
+            {
+                int j = -1;
+                if (p % 3 != 0 && p % 5 != 0)
+                {
+                    for (int a = 2; (a < p) && (j == -1); a++)
+                    {
+                        if ((Math.Pow(a, p - 1) - 1) % p == 0)
+                        {
+                            s++;
+                            Console.WriteLine(p);
+                            j = -j;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApplication11/ConsoleApplication11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine()), a = 2;
+            while (n > 1)
+            {
+                if (n % a == 0)
+                {
+                    n /= a;
+                }
+                else
+                {
+                    a++;
+                }
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
